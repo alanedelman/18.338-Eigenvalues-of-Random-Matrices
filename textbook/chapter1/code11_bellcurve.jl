5f54bf0d108310c64eb7177a421ebd692e2505f5
Manvitha Ponnapati <manvithaponnapati@dhcp-18-111-52-102.dyn.MIT.EDU>
2018-02-18 15:33:34 -0500

    code11

---
+++ textbook/chapter1/code11_bellcurve.jl
@@ -0,0 +1,32 @@
+#=
+bellcurve.jl
+
+ALGORITHM 1.1 of Random Eigenvalues by Alan Edelman
+
+EXPERIMENT  : Generate random samples from the normal distribution
+PLOT        : Histogram of random samples
+THEORY      : The normal distribution curve
+=#
+
+#  ------------ #
+# EXPERIMENT
+#  ------------ #
+t = 10000      #trials
+dx = .2        #binsize
+v = randn(t);  #randn - Generate a normally-distributed random number with mean 0 and standard deviation 1.
+
+#  ------------ #
+# PLOT
+#  ------------ #
+# Install package with
+# Pkg.clone("https://github.com/plotly/Plotly.jl")
+using Plotly
+plotly()
+
+x = -4:dx:4
+histogram(v, xlim=(-4,4), nbins=length(x), normed=true )
+
+#  ------------ #
+# THEORY
+#  ------------ #
+plot!(x,exp.(-x.^2/2)/sqrt(2*pi))
